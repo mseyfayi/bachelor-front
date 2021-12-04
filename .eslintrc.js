2763cbcf8a76fb7cf3886857d840be1451bb3bc3
mohammad.seyfayi <mohammad.seyfayi@tapsi.cab>
2021-12-04 20:20:13 +0330

    config absolute imports

--- .eslintrc.js
+++ .eslintrc.js
@@ -76,4 +76,14 @@ module.exports = {
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'off',
   },
+
+  settings: {
+    // Allow absolute paths in imports, e.g. import Button from 'components/Button'
+    // https://github.com/benmosher/eslint-plugin-import/tree/master/resolvers
+    'import/resolver': {
+      node: {
+        moduleDirectory: ['node_modules', 'src'],
+      },
+    },
+  },
 };
