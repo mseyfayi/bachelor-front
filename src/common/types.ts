b374675c0c38c1fbd3a8b6421fb875547808b0f7
mohammad.seyfayi <mohammad.seyfayi@tapsi.cab>
2021-12-09 17:25:42 +0330

    Blog types

--- src/common/types.ts
+++ src/common/types.ts
@@ -1,6 +1,40 @@
 // todo fix this
 export type Tag = string;
 
-export interface Blog {
+export interface User {
   id: string;
+  githubId: string;
+  beheshtiId: string;
+  firstName: string;
+  lastName: string;
+  avatar: string;
+}
+
+interface Comment {
+  user: User;
+  context: string;
+}
+
+interface BlogFile {
+  name: string;
+  size: string;
+  url: string;
+}
+
+export interface SimpleBlog {
+  id: string;
+  title: string;
+  tags: Array<Tag>;
+  author: Partial<User>;
+  readme: string;
+  likesCount: number;
+  comments: Array<Comment>;
+  files: Array<BlogFile>;
+}
+
+export interface GitBlog extends SimpleBlog {
+  repoTitle: string;
+  description: string;
+  starsCount: number;
+  forksCount: number;
 }
