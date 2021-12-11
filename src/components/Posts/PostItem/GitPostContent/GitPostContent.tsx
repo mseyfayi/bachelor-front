c18a355ece3aedca5cc68d30a52b18fc08040172
mohammad.seyfayi <mohammad.seyfayi@tapsi.cab>
2021-12-11 18:15:35 +0330

    Chronology.tsx redraw onresize

--- src/components/Posts/PostItem/GitPostContent/GitPostContent.tsx
+++ src/components/Posts/PostItem/GitPostContent/GitPostContent.tsx
@@ -3,6 +3,6 @@ import classes from './GitPostContent.module.scss';
 
 type Props = GitPost;
 
-const GitPostContent = (props: Props) => <div className={classes.container}>{props.readme}</div>;
+const GitPostContent = (props: Props) => <div className={classes.container}>{props.content}</div>;
 
 export default GitPostContent;
