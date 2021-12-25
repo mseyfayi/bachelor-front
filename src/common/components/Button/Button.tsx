de4f8d2edc84b4242583e3bac33f3ba62e00ea5a
mohammad.seyfayi <mohammad.seyfayi@tapsi.cab>
2021-12-25 15:38:56 +0330

    Button added

---
+++ src/common/components/Button/Button.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import MuiButton from '@mui/material/Button';
+import { ButtonProps } from '@mui/material/Button/Button';
+import { CircularProgress } from '@mui/material';
+import classes from './Button.module.scss';
+
+interface Props extends ButtonProps {
+  isLoading?: boolean;
+}
+
+function Button({ children, isLoading, onClick, ...restProps }: Props) {
+  return (
+    <MuiButton
+      variant="contained"
+      classes={{
+        root: classes.container,
+      }}
+      {...restProps}
+      onClick={(e) => {
+        if (!isLoading && onClick) onClick(e);
+      }}
+    >
+      {isLoading ? <CircularProgress size={20} color="inherit" /> : children}
+    </MuiButton>
+  );
+}
+
+Button.propTypes = {};
+
+Button.defaultProps = {};
+
+export default Button;
--- src/common/components/index.ts
+++ src/common/components/index.ts
@@ -1,1 +1,4 @@
 export { default as Chronology } from './Chronology';
+export { default as Like } from './Like';
+export { default as TagItem } from './TagItem';
+export { default as Button } from './Button';
