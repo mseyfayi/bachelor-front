850bccd8261d9c77fb407efa03033137fd6e7491
mohammad.seyfayi <mohammad.seyfayi@tapsi.cab>
2021-12-25 15:24:28 +0330

    NewPostPresentation modal

--- src/components/Posts/NewPost/NewPostPresentation.tsx
+++ src/components/Posts/NewPost/NewPostPresentation.tsx
@@ -1,7 +1,32 @@
+import { Dialog, DialogContent, DialogTitle, DialogActions, Button } from '@mui/material';
+import { useState } from 'react';
 import classes from './NewPostPresentation.module.scss';
 
 type Props = {};
 
-const NewPostPresentation = ({}: Props) => <div className={classes.container} />;
+const NewPostPresentation = (props: Props) => {
+  const [open, setOpen] = useState(false);
+  return (
+    <>
+      <Dialog maxWidth="xl" fullWidth open={open} onClose={() => setOpen(false)}>
+        <DialogTitle>پست جدید</DialogTitle>
+        <DialogContent>{/* todo markdown */}koft</DialogContent>
+        <DialogActions>
+          <Button
+            onClick={() => {
+              /* todo */
+            }}
+          >
+            ذخیره
+          </Button>
+          <Button onClick={() => setOpen(false)}>انصراف</Button>
+        </DialogActions>
+      </Dialog>
+      <div className={classes.container} onClick={() => setOpen(true)}>
+        جدید
+      </div>
+    </>
+  );
+};
 
 export default NewPostPresentation;
