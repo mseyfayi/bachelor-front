53415220daff06e0db8fe9e479bb3a418c0331c8
Mohammad Seyfayi <39955233+madsams@users.noreply.github.com>
2021-12-25 19:07:13 +0330

    Rtl (#5)
    * RTL Added
    
    * dialogAction rtl
    
    * wip
    
    Co-authored-by: mohammad.seyfayi <mohammad.seyfayi@tapsi.cab>

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,17 +4,33 @@ import { SnackbarUtilsConfigurator } from 'common/utils';
 import { SnackbarProvider } from 'notistack';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import 'bootstrap/dist/css/bootstrap.css';
+import { createTheme, ThemeProvider } from '@mui/material';
+import { create } from 'jss';
+import rtl from 'jss-rtl';
+import { StylesProvider, jssPreset } from '@mui/styles';
 
 function MyApp({ Component, pageProps }: AppProps) {
   const client = new QueryClient({
     defaultOptions: { queries: { retry: 1, refetchOnWindowFocus: false, refetchOnMount: false } },
   });
 
+  const theme = createTheme({
+    direction: 'rtl',
+  });
+
+  const jss = create({
+    plugins: [...jssPreset().plugins, rtl()],
+  });
+
   return (
     <QueryClientProvider client={client} contextSharing>
       <SnackbarProvider anchorOrigin={{ horizontal: 'center', vertical: 'top' }}>
-        <SnackbarUtilsConfigurator />
-        <Component {...pageProps} />
+        <ThemeProvider theme={theme}>
+          <StylesProvider jss={jss}>
+            <SnackbarUtilsConfigurator />
+            <Component {...pageProps} />
+          </StylesProvider>
+        </ThemeProvider>
       </SnackbarProvider>
     </QueryClientProvider>
   );
