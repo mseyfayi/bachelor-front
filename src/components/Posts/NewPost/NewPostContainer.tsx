b1c92ac3bd828987f7100d15f44e662c57d3229e
mohammad.seyfayi <mohammad.seyfayi@tapsi.cab>
2021-12-25 14:53:25 +0330

    NewPost added to timeline

---
+++ src/components/Posts/NewPost/NewPostContainer.tsx
@@ -0,0 +1,7 @@
+import NewPostPresentation from './NewPostPresentation';
+
+type Props = {};
+
+const NewPostContainer = ({}: Props) => <NewPostPresentation />;
+
+export default NewPostContainer;
---
+++ src/components/Posts/NewPost/NewPostPresentation.tsx
@@ -0,0 +1,7 @@
+import classes from './NewPostPresentation.module.scss';
+
+type Props = {};
+
+const NewPostPresentation = ({}: Props) => <div className={classes.container} />;
+
+export default NewPostPresentation;
