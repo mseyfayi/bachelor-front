518ee2938c3a4b793fa550514d2450294b55d7f0
mohammad.seyfayi <mohammad.seyfayi@tapsi.cab>
2021-12-25 20:40:11 +0330

    Markdown fixed

--- src/components/Posts/NewPost/NewPostPresentation.tsx
+++ src/components/Posts/NewPost/NewPostPresentation.tsx
@@ -1,14 +1,10 @@
-import '@uiw/react-md-editor/markdown-editor.css';
-import '@uiw/react-markdown-preview/markdown.css';
-import dynamic from 'next/dynamic';
 import { Dialog, DialogContent, DialogTitle } from '@mui/material';
 import Button from 'common/components/Button';
 import { useState } from 'react';
 import AddCircleOutlineRoundedIcon from '@mui/icons-material/AddCircleOutlineRounded';
 import { Post } from 'common/types';
+import { MarkDown } from 'common/components';
 import classes from './NewPostPresentation.module.scss';
-
-const MDEditor = dynamic(() => import('@uiw/react-md-editor'), { ssr: false });
 
 type Props = { create: (post: Partial<Post>) => void; isLoading: boolean };
 
@@ -20,8 +16,8 @@ const NewPostPresentation = ({ create, isLoading }: Props) => {
     <>
       <Dialog maxWidth="xl" fullWidth className={classes.dialogContainer} open={open} onClose={() => setOpen(false)}>
         <DialogTitle>پست جدید</DialogTitle>
-        <DialogContent className={classes.dialogContent}>
-          <MDEditor value={content} onChange={(str) => setContent(str ?? '')} />
+        <DialogContent>
+          <MarkDown value={content} onChange={setContent} />
         </DialogContent>
         <footer className={classes.dialogAction}>
           <Button onClick={() => create({ content })} isLoading={isLoading}>
--- src/components/Posts/PostItem/SimplePostContent/SimplePostContent.tsx
+++ src/components/Posts/PostItem/SimplePostContent/SimplePostContent.tsx
@@ -1,11 +1,14 @@
 import { SimplePost } from 'common/types';
+import { MarkDown } from 'common/components';
 import classes from './SimplePostContent.module.scss';
 
 type Props = SimplePost;
 
 const SimplePostContent = (post: Props) => (
   <div className={classes.container}>
-    <div className={classes.content}>{post.content}</div>
+    <div className={classes.content}>
+      <MarkDown value={post.content} preview />
+    </div>
     {/* todo file */}
   </div>
 );
