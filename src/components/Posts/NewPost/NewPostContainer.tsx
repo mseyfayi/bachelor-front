812419d47c79dcbbaf7d2ed8c75fde807aa2ef22
mohammad.seyfayi <mohammad.seyfayi@tapsi.cab>
2022-01-01 15:04:33 +0330

    github link

--- src/components/Posts/NewPost/NewPostContainer.tsx
+++ src/components/Posts/NewPost/NewPostContainer.tsx
@@ -2,11 +2,21 @@ import { useIMutation } from 'common/reactQuery';
 import { Post } from 'common/types';
 import { getPostPostsUrl } from 'common/path';
 import { fetchApi, getPostConfig } from 'common/utils';
+import { useState } from 'react';
 import NewPostPresentation from './NewPostPresentation';
 
 const NewPostContainer = () => {
+  // todo fetch meta
+  const [githubLink, setGithubLink] = useState('');
   const createPostMutation = useIMutation<Partial<Post>>((post) => fetchApi(getPostPostsUrl(), getPostConfig(post)));
-  return <NewPostPresentation isLoading={createPostMutation.isLoading} create={createPostMutation.mutate} />;
+  return (
+    <NewPostPresentation
+      isLoading={createPostMutation.isLoading}
+      create={createPostMutation.mutate}
+      githubLink={githubLink}
+      setGithubLink={setGithubLink}
+    />
+  );
 };
 
 export default NewPostContainer;
--- src/components/Posts/NewPost/NewPostPresentation.tsx
+++ src/components/Posts/NewPost/NewPostPresentation.tsx
@@ -1,22 +1,34 @@
-import { Dialog, DialogContent, DialogTitle } from '@mui/material';
-import Button from 'common/components/Button';
+import { Checkbox, Dialog, DialogContent, DialogTitle, FormControlLabel } from '@mui/material';
+import { Button, MarkDown, TextField } from 'common/components';
 import { useState } from 'react';
 import AddCircleOutlineRoundedIcon from '@mui/icons-material/AddCircleOutlineRounded';
 import { Post } from 'common/types';
-import { MarkDown } from 'common/components';
 import classes from './NewPostPresentation.module.scss';
 
-type Props = { create: (post: Partial<Post>) => void; isLoading: boolean };
+type Props = {
+  create: (post: Partial<Post>) => void;
+  isLoading: boolean;
+  githubLink: string;
+  setGithubLink: (val: string) => void;
+};
 
-const NewPostPresentation = ({ create, isLoading }: Props) => {
+const NewPostPresentation = ({ create, isLoading, githubLink, setGithubLink }: Props) => {
   const [open, setOpen] = useState(false);
   const [content, setContent] = useState('');
+  const [isGithubProject, setGithubProject] = useState(false);
 
   return (
     <>
       <Dialog maxWidth="xl" fullWidth className={classes.dialogContainer} open={open} onClose={() => setOpen(false)}>
         <DialogTitle>پست جدید</DialogTitle>
-        <DialogContent>
+        <DialogContent sx={{ display: 'block' }}>
+          <FormControlLabel
+            control={<Checkbox checked={isGithubProject} onChange={() => setGithubProject((prev) => !prev)} />}
+            label="پروژه گیت‌هاب"
+          />
+          {isGithubProject && (
+            <TextField value={githubLink} onChange={(e) => setGithubLink(e.target.value)} placeholder="لینک مخزن گیت‌هاب" />
+          )}
           <MarkDown value={content} onChange={setContent} />
         </DialogContent>
         <footer className={classes.dialogAction}>
