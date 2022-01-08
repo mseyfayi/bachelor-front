f88b49bc2a3ba03ec3a3bc7a42e45cc9fcfabbe2
mohammad.seyfayi <mohammad.seyfayi@tapsi.cab>
2022-01-08 10:28:23 +0330

    Login added

--- src/common/components/TextField/index.tsx
+++ src/common/components/TextField/index.tsx
@@ -2,19 +2,23 @@ import React from 'react';
 import { Box, CircularProgress, InputAdornment, InputBase, InputBaseProps, InputLabel } from '@mui/material';
 
 interface Props extends InputBaseProps {
+  id?: string;
   isLoading?: boolean;
   disabled?: boolean;
   label?: React.ReactNode;
 }
 
-const TextField = React.forwardRef<HTMLInputElement, Props>(({ label, isLoading, disabled, ...props }, ref) => (
-  <Box sx={{ display: 'flex', alignItems: 'center', my: 1, width: { xs: 1 } }}>
-    <InputLabel>{label}</InputLabel>
-    <Box sx={{ width: { xs: 1 }, px: 2, py: 1, my: 1, borderRadius: 2, border: 1, borderColor: '#ccc' }}>
+const TextField = React.forwardRef<HTMLInputElement, Props>(({ id, label, isLoading, disabled, ...props }, ref) => (
+  <Box sx={{ display: 'flex', alignItems: 'center', mt: 1, width: { xs: 1 } }}>
+    <InputLabel htmlFor={id} sx={{ width: 100 }}>
+      {label}
+    </InputLabel>
+    <Box sx={{ width: { xs: 1 }, my: 1, borderRadius: 2, border: 1, borderColor: '#ccc' }}>
       <InputBase
         {...props}
         ref={ref}
-        sx={{ verticalAlign: 'middle', width: 1 }}
+        id={id}
+        sx={{ verticalAlign: 'middle', width: 1, px: 2, py: 1 }}
         disabled={isLoading || disabled}
         endAdornment={
           isLoading ? (
