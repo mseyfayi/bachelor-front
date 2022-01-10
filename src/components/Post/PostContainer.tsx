ee14cb0282919f2777d246845ce76bffbc81f920
mohammad.seyfayi <mohammad.seyfayi@tapsi.cab>
2022-01-10 23:50:30 +0330

    Post schema

--- src/components/Post/PostContainer.tsx
+++ src/components/Post/PostContainer.tsx
@@ -1,10 +1,29 @@
 import { Post } from 'common/types';
+import { useIQuery } from 'common/reactQuery';
+import { fetchApi, getGetConfig } from 'common/utils';
+import { getGetPostDetailUrl } from 'common/path';
 import PostPresentation from './PostPresentation';
 
 type Props = {
   postId: string;
 };
 
-const PostContainer = ({ postId }: Props) => <PostPresentation {...({} as Post)} />;
+const PostContainer = ({ postId }: Props) => {
+  const { data: post, isLoading } = useIQuery<Post | undefined>(['post', postId], () =>
+    true
+      ? {
+          id: 'id1',
+          tags: [{ id: 'tag1', name: 'امبدد', color: '#421' }],
+          author: {},
+          content: 'string',
+          likesCount: 20,
+          isLiked: true,
+          commentsCount: 12,
+          comments: [],
+        }
+      : fetchApi(getGetPostDetailUrl(postId), getGetConfig()),
+  );
+  return <PostPresentation post={post} isLoading={isLoading} />;
+};
 
 export default PostContainer;
--- src/components/Post/PostPresentation.tsx
+++ src/components/Post/PostPresentation.tsx
@@ -1,6 +1,28 @@
-import { Post } from 'common/types';
-import classes from './PostPresentation.module.scss';
+import { isGitPost, Post } from 'common/types';
+import { Box, CircularProgress } from '@mui/material';
 
-const PostPresentation = (post: Post) => <div className={classes.container} />;
+type Props = {
+  post?: Post;
+  isLoading: boolean;
+};
+const PostPresentation = ({ post, isLoading }: Props) =>
+  !isLoading && post ? (
+    <Box sx={{ p: 4, display: 'flex', flexDirection: ['column', 'row'] }}>
+      <Box sx={{ flex: 1 }}>
+        <p>user info</p>
+        {isGitPost(post) && <p>repo info</p>}
+        <p>tags</p>
+        <Box>
+          <p>like</p>
+          <p>comment</p>
+        </Box>
+      </Box>
+      <Box sx={{ flex: 3 }}>
+        <p>Readme</p> <p>comments</p>
+      </Box>
+    </Box>
+  ) : (
+    <CircularProgress size={18} />
+  );
 
 export default PostPresentation;
