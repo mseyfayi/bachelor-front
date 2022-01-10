6fed67d349beb7c9a09935f2dfb7886be52f52c2
mohammad.seyfayi <mohammad.seyfayi@tapsi.cab>
2022-01-10 23:39:36 +0330

    PostItem treat as secure

--- src/components/Posts/PostItem/PostItem.tsx
+++ src/components/Posts/PostItem/PostItem.tsx
@@ -1,10 +1,11 @@
-import { isGitPost, Post } from 'common/types';
+import { isGitPost, Post, Tag } from 'common/types';
 import { Avatar, IconButton } from '@mui/material';
 import { classnames, fetchApi, getPostConfig, getUsername } from 'common/utils';
 import { Like, TagItem } from 'common/components';
 import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline';
 import { useIMutation } from 'common/reactQuery';
 import { getLikePostUrl, getUnlikePostUrl } from 'common/path';
+import { useRouter } from 'next/router';
 import SimplePostContent from './SimplePostContent';
 import GitPostContent from './GitPostContent';
 import classes from './PostItem.module.scss';
@@ -12,6 +13,7 @@ import classes from './PostItem.module.scss';
 type Props = Post;
 
 const PostItem = (post: Props) => {
+  const router = useRouter();
   const likeMutation = useIMutation(() =>
     fetchApi(post.isLiked ? getUnlikePostUrl(post.id) : getLikePostUrl(post.id), getPostConfig()),
   );
@@ -36,15 +38,15 @@ const PostItem = (post: Props) => {
   );
 
   return (
-    <div className={classes.container}>
+    <div className={classes.container} onClick={() => router.push(`/post/${post.id}`)}>
       <div className="d-flex flex-row align-items-center justify-content-between">
         <div className="d-flex flex-row align-items-center">
           <Avatar src={post.author.avatar} alt={getUsername(post.author)} sx={{ width: 54, height: 54 }} />
           <p className={classes.username}>{getUsername(post.author)}</p>
         </div>
         <div className="d-flex flex-row-reverse flex-wrap align-items-center">
-          {post.tags.map((tag) => (
-            <TagItem key={tag.id} tag={tag} />
+          {(post.tags as Array<Tag>).map((tag) => (
+            <TagItem key={tag?.id} tag={tag} />
           ))}
         </div>
       </div>
