4d73fb75cdfb1c378ed4006a2797baf6cd180601
mohammad.seyfayi <mohammad.seyfayi@tapsi.cab>
2022-01-22 16:59:29 +0330

    ForgetPassword Done

--- src/components/auth/ForgetPassword/ForgetPasswordContainer.tsx
+++ src/components/auth/ForgetPassword/ForgetPasswordContainer.tsx
@@ -1,7 +1,20 @@
+import { useRouter } from 'next/router';
+import { useIMutation } from 'common/reactQuery';
+import { fetchApi, getPostConfig } from 'common/utils';
+import { getForgetPasswordUrl } from 'common/path';
+import type { Data } from './ForgetPasswordPresentation';
 import ForgetPasswordPresentation from './ForgetPasswordPresentation';
 
-type Props = {};
-
-const ForgetPasswordContainer = ({}: Props) => <ForgetPasswordPresentation />;
+const ForgetPasswordContainer = () => {
+  const router = useRouter();
+  const mutation = useIMutation<Data>(
+    (data) => fetchApi(getForgetPasswordUrl(), getPostConfig({ ...data })),
+    'ورود با شکست مواجه شد',
+    {
+      onSuccess: (_, data) => router.push(`/auth/reset-password?email=${data.beheshtiEmail}`),
+    },
+  );
+  return <ForgetPasswordPresentation mutate={mutation.mutate} isLoading={mutation.isLoading} />;
+};
 
 export default ForgetPasswordContainer;
--- src/components/auth/ForgetPassword/ForgetPasswordPresentation.tsx
+++ src/components/auth/ForgetPassword/ForgetPasswordPresentation.tsx
@@ -1,7 +1,43 @@
-import classes from './ForgetPasswordPresentation.module.scss';
+import { useRouter } from 'next/router';
+import Form, { Fields } from 'common/components/Form';
+import * as yup from 'yup';
+import { Button } from '@mui/material';
 
-type Props = {};
+export type Data = {
+  beheshtiEmail: string;
+};
 
-const ForgetPasswordPresentation = ({}: Props) => <div className={classes.container} />;
+type Props = {
+  mutate: (data: Data) => void;
+  isLoading: boolean;
+};
+
+const ForgetPasswordPresentation = ({ mutate, isLoading }: Props) => {
+  const router = useRouter();
+
+  const fields: Fields<Data> = {
+    beheshtiEmail: {
+      props: {
+        dir: 'ltr',
+        label: 'ایمیل بهشتی',
+        placeholder: 'example@mail.sbu.ac.ir',
+      },
+      validation: yup
+        .string()
+        .matches(/^\S+@(mail\.)?sbu\.ac\.ir$/, 'ایمیل باید تحت دامنه بهشتی باشد')
+        .required('ایمیل بهشتی اجباری است'),
+    },
+  };
+
+  return (
+    <Form
+      fields={fields}
+      submitLabel="بازنشانی"
+      mutation={mutate}
+      isLoading={isLoading}
+      footerButton={<Button onClick={() => router.push('/auth/login')}>رمز عبور خود را به خاطر دارید؟</Button>}
+    />
+  );
+};
 
 export default ForgetPasswordPresentation;
