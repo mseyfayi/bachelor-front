9ecdc914094cc7a56a302152ed397aa11a8907f5
mohammad.seyfayi <mohammad.seyfayi@tapsi.cab>
2022-01-22 15:05:02 +0330

    githubId added to signup

--- src/components/auth/SignUp/SignUpContainer.tsx
+++ src/components/auth/SignUp/SignUpContainer.tsx
@@ -5,7 +5,9 @@ import SignUpPresentation from './SignUpPresentation';
 import type { Data } from './SignUpPresentation';
 
 const SignUpContainer = () => {
-  const mutation = useIMutation<Data>((data) => fetchApi(getSignUpUrl(), getPostConfig({ ...data })));
+  const mutation = useIMutation<Omit<Data, 'confirmPassword'>>((data) =>
+    fetchApi(getSignUpUrl(), getPostConfig({ ...data, confirmPassword: undefined })),
+  );
 
   return <SignUpPresentation isLoading={mutation.isLoading} mutation={mutation.mutate} />;
 };
