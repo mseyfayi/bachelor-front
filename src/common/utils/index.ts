3de039341ca91b53ac5aec7aa9643b9b5906f916
mohammad.seyfayi <mohammad.seyfayi@tapsi.cab>
2022-01-25 12:24:30 +0330

    countDown removed

--- src/common/utils/index.ts
+++ src/common/utils/index.ts
@@ -1,5 +1,4 @@
 export * from './localStorage';
 export * from './fetch';
 export * from './snackbar';
-export * from './hooks';
 export * from './utils';
--- src/components/auth/ConfirmCode/ConfirmCodePresentation.tsx
+++ src/components/auth/ConfirmCode/ConfirmCodePresentation.tsx
@@ -1,6 +1,5 @@
 import CodeInput from 'react-verification-code-input';
 import { Box, Typography } from '@mui/material';
-import { useCountDown } from 'common/utils';
 import classes from './ConfirmCodePresentation.module.scss';
 
 type Props = {
@@ -8,14 +7,11 @@ type Props = {
   isLoading: boolean;
 };
 
-const ConfirmCodePresentation = ({ mutate, isLoading }: Props) => {
-  const count = useCountDown(120);
-  return (
-    <Box className={classes.container}>
-      <Typography>کد تایید ایمیل شده را وارد کنید</Typography>
-      <CodeInput type="number" className={classes.codeInput} fields={5} onComplete={mutate} loading={isLoading} autoFocus />
-    </Box>
-  );
-};
+const ConfirmCodePresentation = ({ mutate, isLoading }: Props) => (
+  <Box className={classes.container}>
+    <Typography>کد تایید ایمیل شده را وارد کنید</Typography>
+    <CodeInput type="number" className={classes.codeInput} fields={5} onComplete={mutate} loading={isLoading} autoFocus />
+  </Box>
+);
 
 export default ConfirmCodePresentation;
